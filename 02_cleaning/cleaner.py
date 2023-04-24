5f66395a0fc3f809fc2e2cb4a4a58c5d21f2eae5
justinhjy1004@github.com <justinhjy1004@gmail.com>
2023-04-24 15:42:21 -0500

    save just in case

--- 02_cleaning/cleaner.py
+++ 02_cleaning/cleaner.py
@@ -1,41 +1,5 @@
 import re
 
-def clean_plaintiff(plaintiff, plaintiff_attorney):
-    
-    if type(plaintiff_attorney) != str:
-        return plaintiff
-    
-    x = re.search("(.*?) [\s]{2,} (.*?)", plaintiff_attorney)
-    
-    if x is None:
-        return plaintiff
-    else:
-        return str(plaintiff) + str(x.group(1))
-
-def clean_plaintiff_attorney(plaintiff_attorney):
-    
-    if type(plaintiff_attorney) != str:
-        return plaintiff_attorney
-    
-    x = re.search("(.*?) [\s]{2,} (.*?)", plaintiff_attorney)
-
-    if x is None:
-        return plaintiff_attorney
-    else:
-        return re.sub(x.group(1), "", plaintiff_attorney).strip()
-    
-def clean_defendant_address(defendant_attorney, defendants_address):
-
-    if (type(defendant_attorney) != str) or (type(defendants_address) != str):
-        return defendants_address
-    
-    x = re.search(defendant_attorney + ", (.*?) [0-9]{5}", defendants_address)
-
-    if x is None:
-        return defendants_address
-    else:
-        return re.sub(defendant_attorney + ", ", "",x.group(0))
-    
 def judgement_of_restitution(judgement_info):
 
     if type(judgement_info) != str:
@@ -77,3 +41,99 @@ def defendant_and_attorney(parties):
     attorney = x.group(3).strip()
 
     return defendant, attorney
+
+"""
+Identify correctly Defendant and Attorney
+Input: Parties
+Output: Defendant and Attorney
+"""
+def defendant_and_attorney(parties):
+
+    if type(parties) != str:
+        return None, None
+    
+    x = re.search("Defendant (.*?)[\s]{3,}(.*?)[^\S\r\n]{2,}(.*?)\n", parties)
+
+    if x is None:
+        return None, None
+    
+    defendant = x.group(2).strip()
+    attorney = x.group(3).strip()
+
+    return defendant, attorney
+
+"""
+Helper function for get_address()
+Input: text
+Output: return the first half of the text
+"""
+def get_half(text):
+    n = len(text)
+    half = round(n/2)
+    
+    return text[:half]
+
+"""
+Obtain Defendant's Address
+Input: Defendant, Attorney and Parties
+Output: Defendant's address
+"""
+def get_address(defendant, defendant_attorney, parties):
+
+    # Flag for attorney
+    has_attorney = True
+
+    # Type checking
+    if type(defendant) != str:
+        return None
+    
+    if type(defendant_attorney) != str:
+        defendant_attorney = ""
+        has_attorney = False
+
+    if type(parties) != str:
+        return None
+    
+    # Relabel parantheses
+    defendant = re.sub("\(", "\(", defendant)
+    defendant = re.sub("\)", "\)", defendant)
+
+    # Base matching format
+    matching_format = defendant + r"[^\S\r\n]{2,}" + defendant_attorney + r"[\S\s]* NE \d{5}"
+    
+    x = re.search(matching_format, parties)
+
+    # Match on different specifications
+    if x is None:
+        # Without ZIP Code
+        matching_format = defendant + "[^\S\r\n]{1,}" + defendant_attorney + "[\S\s]* NE"
+        x = re.search(matching_format, parties)
+        if x is None:
+            # One line address with ZIP
+            matching_format = defendant + "(.*?) NE \d{5}"
+            x = re.search(matching_format, parties)
+            if x is None:
+                # One line address withOUT ZIP
+                matching_format = defendant + "(.*?) NE"
+                x = re.search(matching_format, parties)
+                if x is None:
+                    return None
+
+    # Remove defendant and attorney from address
+    address = x.group(0)
+    address = re.sub(defendant, "", address).strip()
+    address = re.sub(defendant_attorney, "", address).strip()
+
+    # If has attorney, roughly get half
+    if has_attorney:
+        address = "".join(list(map(get_half,address.split("\n"))))
+
+    address = re.sub("[\\s]{3,}", ", ", address)
+ 
+    address = address.split(", Defendant ACTIVE, ")[0]
+    address = address.split(", Witness ACTIVE, ")[0]
+    address = address.split(", owes")[0]
+    address = address.split(", Limited Representation Attorney")[0]
+    
+    return address  
+    
--- 02_cleaning/cleaning.py
+++ 02_cleaning/cleaning.py
@@ -7,12 +7,10 @@ if __name__ == '__main__':
 
     df = pd.read_csv(file)
 
-    df.PLAINTIFF = df.apply(lambda x: cleaner.clean_plaintiff(x.PLAINTIFF, x.PLAINTIFF_ATTORNEY), axis=1)
-    df.PLAINTIFF_ATTORNEY = df.PLAINTIFF_ATTORNEY.apply(cleaner.clean_plaintiff_attorney)
-    df.DEFENDANTS_ADDRESS = df.apply(lambda x: cleaner.clean_defendant_address(x.DEFENDANT_ATTORNEY, x.DEFENDANTS_ADDRESS), axis=1)
-    df.RESTITUTION = df.JUDGEMENT_INFO.apply(cleaner.judgement_of_restitution)
-
+    df["RESTITUTION"] = df.JUDGEMENT_INFO.apply(cleaner.judgement_of_restitution)
     df["PLAINTIFF"], df["PLAINTIFF_ATTORNEY"] = zip(*df.PARTIES.apply(cleaner.plaintiff_and_attorney))
     df["DEFENDANT"], df["DEFENDANT_ATTORNEY"] = zip(*df.PARTIES.apply(cleaner.defendant_and_attorney))
+    
+    df["DEFENDANTS_ADDRESS"] = df.apply(lambda x: cleaner.get_address(x.DEFENDANT, x.DEFENDANT_ATTORNEY, x.PARTIES), axis=1)
 
     df.to_csv("clean_" + file, index=None)
